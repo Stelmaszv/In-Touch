413472b189877ed4f7bbadcee2def12eca3d3130
test <MY_NAME@example.com>
2020-07-08 15:25:09 +0200

    add assets

--- heroku/urls.py
+++ heroku/urls.py
@@ -31,6 +31,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
