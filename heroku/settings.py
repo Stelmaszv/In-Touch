6e5f2a787c5331a816d4159a8862657d761d4246
test <MY_NAME@example.com>
2020-07-08 15:03:03 +0200

    new data

--- heroku/settings.py
+++ heroku/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'heroku.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templete')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
