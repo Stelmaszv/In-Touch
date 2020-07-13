4bb46a63992f997c5fca8492e8b7d2f12f7cc6db
test <MY_NAME@example.com>
2020-07-13 19:52:53 +0200

    new setings

--- backend/settings.py
+++ backend/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'deploy')
+STATIC_ROOT = os.path.join(BASE_DIR, '/deploy')
 STATIC_URL = '/deploy/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'deploy/assets')
+MEDIA_URL = '/assets/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, nenv_manage.DEPLOY),
+)
