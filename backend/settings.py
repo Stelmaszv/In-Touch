7b9a541b5ee827911f60552c165cde78b130ee5a
test <MY_NAME@example.com>
2020-07-13 14:04:28 +0200

    new static file

--- backend/settings.py
+++ backend/settings.py
@@ -116,4 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT =os.path.join(BASE_DIR,'backend/static')
 
+STATIC_URL='backend/static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'backend/static'),
+)
