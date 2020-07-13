c1e6f82115e3cf4ec5a4e9f7fd1643fc0d72367f
test <MY_NAME@example.com>
2020-07-13 18:37:31 +0200

    updata setings.py

--- backend/settings.py
+++ backend/settings.py
@@ -116,19 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/assets/'
 
 
-STATIC_ROOT =os.path.join(BASE_DIR,'/static')
-
-STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static'),
-)
 
 
