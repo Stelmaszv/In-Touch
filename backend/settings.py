35f28bb9ac1ece5e95a788e168499fd5ee538aeb
test <MY_NAME@example.com>
2020-07-13 14:10:43 +0200

    fixed error with static files

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from nenvdata.nenv import nenv_manage
+from backend.nenvdata.nenv import nenv_manage
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT =os.path.join(BASE_DIR,'backend/static')
 
-STATIC_URL='backend/static'
+STATIC_URL='/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'backend/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
