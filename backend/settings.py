8684e73b9939db8c408e6de1c7ad6d0f5fcadf0a
test <MY_NAME@example.com>
2020-07-13 18:30:23 +0200

    new STATIC_ROOT

--- backend/settings.py
+++ backend/settings.py
@@ -121,7 +121,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/assets/'
 
 
-STATIC_ROOT =os.path.join(BASE_DIR,'static')
+STATIC_ROOT =os.path.join(BASE_DIR,'/static')
 
 STATIC_URL = '/static/'
 
