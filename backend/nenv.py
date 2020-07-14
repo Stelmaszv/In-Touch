44ed2866f9a3270bb1006bf7f05c2a66b6a26e24
test <MY_NAME@example.com>
2020-07-14 16:55:32 +0200

    set debug False

--- backend/nenv.py
+++ backend/nenv.py
@@ -4,7 +4,7 @@ class nenv_manage:
     SECRET_KEY = '^$pm=xwj1znq+vm3pfm$j7%3^+0m5l-msgg8r9gm7pe06ft3&k'
     DEBUG  = True
     DEPLOY = 'deploy'
-    DEPLOY_SETINGS = True
+    DEPLOY_SETINGS = False
     #end herocu setings
     def __init__(self):
         self.set_DEBUG()
