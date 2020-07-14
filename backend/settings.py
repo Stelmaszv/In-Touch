529937ed6b20fbb8b6e49e80ad6edb42a6aee4c8
test <MY_NAME@example.com>
2020-07-14 14:25:08 +0200

    new requirements.txt

--- backend/settings.py
+++ backend/settings.py
@@ -46,8 +46,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
