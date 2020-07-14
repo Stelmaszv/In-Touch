4228b7f461d37b0cc82b3557b214616d5bf12b23
test <MY_NAME@example.com>
2020-07-14 13:53:54 +0200

    add nenv_manage

--- backend/nenv.py
+++ backend/nenv.py
@@ -2,13 +2,11 @@ class nenv_manage:
     #start herocu defult setings
     ALLOWED_HOSTS = ['intouchpl.herokuapp.com']
     SECRET_KEY = '^$pm=xwj1znq+vm3pfm$j7%3^+0m5l-msgg8r9gm7pe06ft3&k'
-    DEBUG  = False
-    DEPLOY = 'deploy'
+    DEBUG  = True
     #end herocu setings
     def __init__(self):
         self.set_DEBUG()
     def set_DEBUG(self):
         if self.DEBUG:
             self.ALLOWED_HOSTS=[]
-            self.DEPLOY = '/deploy'
         return self.ALLOWED_HOSTS
