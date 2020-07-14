9ecd98aa23fd1e88f2823ed500de30a7ad55d0ef
test <MY_NAME@example.com>
2020-07-14 13:49:44 +0200

    fixed error 404 (Not Found)

--- backend/settings.py
+++ backend/settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from backend.nenv import nenv_manage
-nenv_manage=nenv_manage()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,12 +18,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = nenv_manage.SECRET_KEY
+SECRET_KEY = '56wbfpfti#*+uy!d&lwoo^j0l-1eu8qbc5c^c%hl2e4qblgf2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = nenv_manage.DEBUG
+DEBUG = True
 
-ALLOWED_HOSTS = nenv_manage.ALLOWED_HOSTS
+ALLOWED_HOSTS = ['https://intouchpl.herokuapp.com/']
 
 
 # Application definition
