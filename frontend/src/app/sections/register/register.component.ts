296c3bd181151e587abeba15c8199e03eb5e8345
test <MY_NAME@example.com>
2020-08-03 13:11:25 +0200

    new  commun  style "section-center"

--- frontend/src/app/sections/register/register.component.ts
+++ frontend/src/app/sections/register/register.component.ts
@@ -1,18 +1,7 @@
 import {Component} from '@angular/core';
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
 
-interface Car {
-  value: string;
-  viewValue: string;
-}
 
-/**
- * @title Select in a form
- */
 
 @Component({
   selector: 'app-register',
@@ -20,18 +9,7 @@ interface Car {
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  selectedValue: string;
-  selectedCar: string;
 
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
 
-  cars: Car[] = [
-    {value: 'volvo', viewValue: 'Volvo'},
-    {value: 'saab', viewValue: 'Saab'},
-    {value: 'mercedes', viewValue: 'Mercedes'}
-  ];
+
 }
