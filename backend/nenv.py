658249764009a1815eaeb3b03b42af12464b3aec
test <MY_NAME@example.com>
2020-08-11 17:11:49 +0200

    set Deploy True

--- backend/nenv.py
+++ backend/nenv.py
@@ -4,7 +4,7 @@ class nenv_manage:
     SECRET_KEY = '^$pm=xwj1znq+vm3pfm$j7%3^+0m5l-msgg8r9gm7pe06ft3&k'
     DEBUG  = True
     DEPLOY = 'deploy'
-    DEPLOY_SETINGS = True # True for localhost False for herocu
+    DEPLOY_SETINGS = False # True for localhost False for herocu
     #end herocu setings
     def __init__(self):
         self.set_DEBUG()
